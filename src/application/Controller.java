0e6ff70910c92a43743ab6cbdb4a3623e46d7c0e
davisp_sequoia <davisp_sequoia@localhost>
2026-08-05 20:42:17 -0600

    20:42 video1 10:47

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -21,7 +21,16 @@ public class Controller {
 		if (form.a.getX() + MOVE <= XMAX - SIZE && form.b.getX() + MOVE <= XMAX - SIZE
 				&& form.c.getX() + MOVE <= XMAX - SIZE && form.d.getX() + MOVE <= XMAX - SIZE) {
 			int movea = MESH[((int) form.a.getX() / SIZE) + 1][((int) form.a.getY() / SIZE)];
+			int moveb = MESH[((int) form.b.getX() / SIZE) + 1][((int) form.b.getY() / SIZE)];
+			int movec = MESH[((int) form.c.getX() / SIZE) + 1][((int) form.c.getY() / SIZE)];
+			int moved = MESH[((int) form.d.getX() / SIZE) + 1][((int) form.d.getY() / SIZE)];
+			if (movea == 0 && movea == moveb && moveb == movec && movec == moved) {
+				form.a.setX(form.a.getX() + MOVE);
+				form.b.setX(form.b.getX() + MOVE);
+				form.c.setX(form.c.getX() + MOVE);
+				form.d.setX(form.d.getX() + MOVE);
+
+			}
 		}
 	}
 }
-// adsjfladsj
