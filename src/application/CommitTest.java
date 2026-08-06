d28ee08b924a404ebdaa3dc3c4ca0593215fe8d9
davisp_sequoia <davisp_sequoia@localhost>
2026-08-05 19:50:16 -0600

    19:50 commit test

---
+++ src/application/CommitTest.java
@@ -0,0 +1,15 @@
+package application;
+
+// This is to test if committing a certain way will keep old changes w/ new changes, or overwrite entirely.
+
+public class CommitTest {
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+}
